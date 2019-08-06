d40ce2e5ac2d645f5fdaebdbac424b6ede4a123a
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-08-06 16:17:36 -0300

    R  route.php -> call.php

--- call.php
+++ call.php
@@ -1,7 +1,7 @@
 <?php
 //Criado por Anderson Ismael
 //21 de junho de 2019
-function route($name){
+function call($name){
     $filename=ROOT.'call/getcall/'.$name.'/index.php';
     if(file_exists($filename)){
         require $filename;
